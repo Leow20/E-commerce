982180548183033629c9f6b04a56afe457ece105
Lucas Hack Costa Beber <lucashackcostabeber@gmail.com>
2023-07-04 14:52:26 -0300

    First commit

--- project-ecommerce/src/App.jsx
+++ project-ecommerce/src/App.jsx
@@ -1,7 +1,9 @@
+import Footer from "./components/footer";
 function App() {
   return (
     <>
       <h1>Ola</h1>
+      <Footer />
     </>
   );
 }
---
+++ project-ecommerce/src/components/Footer/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import "./footer.css"
+
+
+function Footer() {
+  return (
+    <div className='container-footer'>
+      <div className='coluna'>
+        <h4>Shop by Category</h4>
+        <a href="#">Skincare</a>
+        <a href="#">Personal Care</a>
+        <a href="#">Handbags</a>
+        <a href="#">Appareis</a>
+        <a href="#">Watches</a>
+        <a href="#">Eye Wear</a>
+        <a href="#">Jewellery</a>
+      </div>
+
+      <div className='coluna'>
+        <h4>About</h4>
+        <a href="#">Contact Us</a>
+        <a href="#">About Us</a>
+        <a href="#">Careers</a>
+        <a href="#">Press</a>
+      </div>
+
+      <div className='coluna'>
+        <h4>Policy</h4>
+        <a href="#">Return Policy</a>
+        <a href="#">Terms of Use</a>
+        <a href="#">Sitemap</a>
+        <a href="#">Security</a>
+        <a href="#">Security</a>
+        <a href="#">EPR Compliance</a>
+      </div>
+
+      <div className='logos'>
+        
+      </div>
+    </div>
+  )
+}
+
+export default Footer
