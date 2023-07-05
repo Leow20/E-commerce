f84e74d3e3b55e5772003a1f3cdb3b63a63c4d62
Patrick <patrickprestes977@gmail.com>
2023-07-05 16:23:58 -0300

    header desktop

--- project-ecommerce/src/components/Header/index.jsx
+++ project-ecommerce/src/components/Header/index.jsx
@@ -1,10 +1,16 @@
 import "./header.css";
+import search from "../../assets/search.svg"
+import wishlist from "../../assets/wishlist.svg";
+import perfil from "../../assets/profile.svg";
+import sacola from "../../assets/bag.svg";
+import logo from "../../assets/logo.png";
+
 
 function Header() {
     return (
         <header className="Header">
             <div className="logo-header">
-                <span>CORA'L</span>
+                <img src={logo} alt="logo" />
             </div>
             <nav className="nav-category-header">
                 <ul>
@@ -17,9 +23,12 @@ function Header() {
             </nav>
             <div className="search-bar-header">
                 <input type="text" placeholder="Search for products or brands....." />
-
+                <img src={search} alt="lupa" id="lupa-id" />
             </div>
             <div className="icons">
+                <img src={wishlist} alt="coracao" />
+                <img src={perfil} alt="perfil" />
+                <img src={sacola} alt="sacola" />
 
             </div>
         </header>
