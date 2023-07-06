59922ab779275dad01b02a7070dc3ad03d3b9121
Leonardo Winter <leonardo.2020305040@aluno.iffar.edu.br>
2023-07-06 15:46:41 -0300

    Search Bar responsive

--- project-ecommerce/src/components/Header/index.jsx
+++ project-ecommerce/src/components/Header/index.jsx
@@ -1,54 +1,66 @@
 import "./header.css";
-import search from "../../assets/search.svg"
-import wishlist from "../../assets/wishlist.svg"
-import perfil from "../../assets/profile.svg"
-import sacola from "../../assets/bag.svg"
-import logo from "../../assets/logo.png"
-import notificacao from "../../assets/notification.svg"
-import menu from "../../assets/leadingIcon.svg"
-import addHome from "../../assets/addHomes.svg"
+import search from "../../assets/search(1).svg";
+import wishlist from "../../assets/wishlist.svg";
+import perfil from "../../assets/profile.svg";
+import sacola from "../../assets/bag.svg";
+import logo from "../../assets/logo.png";
+import notificacao from "../../assets/notification.svg";
+import menu from "../../assets/leadingIcon.svg";
+import addHome from "../../assets/addHomes.svg";
 import { Link } from "react-router-dom";
 
-
 function Header() {
-    return (
-        <header className="Header">
-            <div className="logo-header">
-                <Link to="/"><img src={logo} alt="logo" /></Link>
-            </div>
-            <nav className="nav-category-header">
-                <ul>
-                    <li><Link to="/handbags">Handbags</Link></li>
-                    <li><Link to="/watches">Watches</Link></li>
-                    <li><Link to="/skincare">Skincare</Link></li>
-                    <li><Link to="/jewellery">Jewellery</Link></li>
-                    <li><Link to="/apparels">Apparels</Link></li>
-                </ul>
-            </nav>
-            <div className="search-bar-header">
-                <input type="text" placeholder="Search for products or brands....." />
-                <img src={search} alt="lupa" id="lupa-id" />
-            </div>
-            <div className="icons">
-                <img src={wishlist} alt="coracao" />
-                <img src={perfil} alt="perfil" />
-                <img src={sacola} alt="sacola" />
-            </div>
+  return (
+    <div className="page-wrapper-header">
+      <header className="Header">
+        <div className="logo-header">
+          <Link to="/">
+            <img src={logo} alt="logo" />
+          </Link>
+        </div>
+        <nav className="nav-category-header">
+          <ul>
+            <li>
+              <Link to="/handbags">Handbags</Link>
+            </li>
+            <li>
+              <Link to="/watches">Watches</Link>
+            </li>
+            <li>
+              <Link to="/skincare">Skincare</Link>
+            </li>
+            <li>
+              <Link to="/jewellery">Jewellery</Link>
+            </li>
+            <li>
+              <Link to="/apparels">Apparels</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="search-bar-header">
+          <input type="text" placeholder="Search for products or brands....." />
+          <img src={search} alt="lupa" id="lupa-id" />
+        </div>
+        <div className="icons">
+          <img src={wishlist} alt="coracao" />
+          <img src={perfil} alt="perfil" />
+          <img src={sacola} alt="sacola" />
+        </div>
 
-            <div className="container-header-responsivo">
-                <div className="logo-menu">
-                    <h1>Home</h1>
-                    <img src={menu} alt="menu" id="menu" />
-                </div>
-                <div className="icons-responsivo">
-                    <img src={addHome} alt="addHome" />
-                    <img src={search} alt="lupa" />
-                    <img src={notificacao} alt="notificacao" />
-                </div>
-            </div>
-
-        </header>
-    );
+        <div className="container-header-responsivo">
+          <div className="logo-menu">
+            <h1>Home</h1>
+            <img src={menu} alt="menu" id="menu" />
+          </div>
+          <div className="icons-responsivo">
+            <img src={addHome} alt="addHome" />
+            <img src={search} alt="lupa" />
+            <img src={notificacao} alt="notificacao" />
+          </div>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default Header;
