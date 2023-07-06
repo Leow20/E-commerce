f04f9fba78f8bedf0b703ecbbb7ac32d4fac7465
William Teuschel Klein <williamklein08@gmail.com>
2023-07-06 14:02:37 -0300

    modal do header

--- project-ecommerce/src/Pages/Home/index.jsx
+++ project-ecommerce/src/Pages/Home/index.jsx
@@ -3,15 +3,11 @@ import Footer from "../../components/footer";
 const Home = () => {
 	return (
 		<div>
-
 			<h1>Home</h1>
 
 			<Footer />
-
 		</div>
-
-	)
-
+	);
 };
 
 export default Home;
---
+++ project-ecommerce/src/components/HeaderModal/index.jsx
@@ -0,0 +1,106 @@
+import React from "react";
+import "./headerModal.css";
+import { Link } from "react-router-dom";
+import arrowBlack from "../../assets/HeaderModal/arrow-right-black.svg";
+import arrowGray from "../../assets/HeaderModal/arrow-right-gray.svg";
+import userNotPicture from "../../assets/HeaderModal/user-sem-foto.png";
+
+const HeaderModal = ({ isOpen }) => {
+	if (isOpen) {
+		return (
+			<div className="header-modal">
+				<div className="content-header-modal">
+					<div className="profile-header-modal">
+						<button>
+							<div>
+								<img src={userNotPicture} alt="User" id="imgUserId" />
+								<h1>Hello </h1>
+							</div>
+							<img src={arrowBlack} alt="arrow-icon" />
+						</button>
+					</div>
+					<hr />
+					<nav className="categories-header-modal">
+						<h2>Top Categories</h2>
+						<ul>
+							<li>
+								<Link to="/skincare">
+									<label>Skincare</label>
+									<img src={arrowGray} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link to="/apparels">
+									<label>Apparels</label>
+									<img src={arrowGray} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link to="/jewellery">
+									<label>Jewellery</label>
+									<img src={arrowGray} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link to="/handbags">
+									<label>Handbags</label>
+									<img src={arrowGray} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link to="">
+									<label>Eyeware</label>
+									<img src={arrowGray} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link to="">
+									<label>Fragrance</label>
+									<img src={arrowGray} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link to="/watches">
+									<label>Watches</label>
+									<img src={arrowGray} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link to="/about">
+									<label>About</label>
+									<img src={arrowGray} alt="arrow-icon" />
+								</Link>
+							</li>
+						</ul>
+					</nav>
+					<hr />
+					<nav className="contact-header-mobile">
+						<h2>Contact Us</h2>
+						<ul>
+							<li>
+								<Link>
+									<label>Help & Support</label>
+									<img src={arrowBlack} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link>
+									<label>Feedback & Suggestions</label>
+									<img src={arrowBlack} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link>
+									<label>Visit Websites</label>
+									<img src={arrowBlack} alt="arrow-icon" />
+								</Link>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		);
+	}
+};
+
+export default HeaderModal;
