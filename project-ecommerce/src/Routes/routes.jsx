2ab874e07f066492b24300b6f5413c50df42d26f
Leonardo Winter <leonardo.2020305040@aluno.iffar.edu.br>
2023-07-06 17:58:10 -0300

    Routes header

--- project-ecommerce/src/Routes/routes.jsx
+++ project-ecommerce/src/Routes/routes.jsx
@@ -8,19 +8,19 @@ import Jewellery from "../Pages/Jewellery";
 import Apparels from "../Pages/Apparels";
 
 const Router = () => {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/handbags" element={<Handbags />} />
-				<Route path="/watches" element={<Watches />} />
-				<Route path="/skincare" element={<Skincare />} />
-				<Route path="/jewellery" element={<Jewellery />} />
-				<Route path="/apparels" element={<Apparels />} />
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/handbags" element={<Handbags />} />
+        <Route path="/watches" element={<Watches />} />
+        <Route path="/skincare" element={<Skincare />} />
+        <Route path="/jewellery" element={<Jewellery />} />
+        <Route path="/apparels" element={<Apparels />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default Router;
