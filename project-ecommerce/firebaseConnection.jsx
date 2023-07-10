ea46ce30ad59d098eab56511f6f56e41b9dd91c6
Leonardo Winter <leonardo.2020305040@aluno.iffar.edu.br>
2023-07-10 18:46:27 -0300

    feat: add firebase conection

---
+++ project-ecommerce/firebaseConnection.jsx
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCiIfd2XGh0dlwHiwYuSFUQADXLeMXe2Rw",
+  authDomain: "e-commerce-b5c15.firebaseapp.com",
+  projectId: "e-commerce-b5c15",
+  storageBucket: "e-commerce-b5c15.appspot.com",
+  messagingSenderId: "95494243001",
+  appId: "1:95494243001:web:07543fedf6da705135d168",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+
+export { db, auth };
