ebf6f1d8a7e72030fb4f60834e3bc415b8075a56
William Teuschel Klein <williamklein08@gmail.com>
2023-07-11 00:11:39 -0300

    feat: add login page

--- project-ecommerce/src/Pages/Login/index.jsx
+++ project-ecommerce/src/Pages/Login/index.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
+
+//Style
+import "./login.css";
+
+//Router-dom
+import { Link } from "react-router-dom";
 
 const Login = () => {
-	return <div>Login</div>;
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const handleLogin = (e) => {
+		e.preventDefault();
+	};
+	return (
+		<main className="login-page">
+			<div className="gradient-top-login"></div>
+			<div className="container-login">
+				<div className="box-login">
+					<div className="box-text-login">
+						<h1>Welcome!</h1>
+						<p>Please enter your credentials to access your account.</p>
+					</div>
+					<form onSubmit={() => handleLogin}>
+						<div>
+							<input
+								type="text"
+								name="emailInput"
+								id="emailId"
+								onChange={(e) => setEmail(e.target.value)}
+								required="requerid"
+							/>
+							<span>Email</span>
+						</div>
+						<div>
+							<input
+								type="password"
+								name="passwordInput"
+								id="passwordId"
+								onChange={(e) => setPassword(e.target.value)}
+								required="requerid"
+							/>
+							<span>Password</span>
+						</div>
+						<button type="submit">Login</button>
+						<label className="have-acount-login">
+							Don't have an account yet? <Link>Register</Link>
+						</label>
+					</form>
+				</div>
+			</div>
+			<div className="gradient-bottom-login"></div>
+		</main>
+	);
 };
 
 export default Login;
