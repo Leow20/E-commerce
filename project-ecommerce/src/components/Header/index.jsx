06caf17eb7ce09d090adb29696115062517e4367
Leonardo Winter <leonardo.2020305040@aluno.iffar.edu.br>
2023-07-20 17:16:02 -0300

    feat/dark background on modal

--- project-ecommerce/src/components/Header/index.jsx
+++ project-ecommerce/src/components/Header/index.jsx
@@ -15,20 +15,15 @@ import logo from "../../assets/imgHeader/logo.png";
 import notificacao from "../../assets/imgHeader/notification.svg";
 import menu from "../../assets/imgHeader/leadingIcon.svg";
 import addHome from "../../assets/imgHeader/addHomes.svg";
-import arrow from "../../assets/icons/arroBlue.svg";
-import bolsa from "../../assets/Img/bolsa_rosa.jpg";
-import cross from "../../assets/icons/cross-small.svg";
+import MyBagModal from "../myBagModal";
 
 function Header({ Page }) {
   const [modal, setModal] = useState(false);
-  const [isHovered, setIsHovered] = useState(false);
+  const [isHovered, setIsHovered] = useState();
 
   const handleHover = () => {
-    setIsHovered(true);
-  };
-
-  const handleMouseLeave = () => {
-    setIsHovered(false);
+    setIsHovered(!isHovered);
+    console.log("passo" + isHovered);
   };
 
   return (
@@ -69,7 +64,7 @@ function Header({ Page }) {
               placeholder="Search for products or brands....."
             />
           </div>
-          <div className="icons">
+          <div className="icons-header">
             <Link to="/wishlist">
               <img src={wishlist} alt="coracao" />
             </Link>
@@ -79,120 +74,15 @@ function Header({ Page }) {
             </Link>
 
             <Link to="/mybag">
-              <img src={sacola} alt="sacola" onMouseEnter={handleHover} />
+              <img src={sacola} alt="sacola" />
+              <div className="container-bag-icon" onMouseEnter={handleHover}>
+                {" "}
+                <img src={sacola} alt="sacola" />
+              </div>
             </Link>
           </div>
 
-          {isHovered && (
-            <div className="modal-mybag-header">
-              <div className="mybag-btnh2-header">
-                <button onClick={handleMouseLeave}>
-                  <img src={arrow} alt="Back" />
-                </button>
-                <h2>Back</h2>
-              </div>
-
-              <div className="max-product-header">
-                <div className="mybag-container-product-header">
-                  <div className="img-product-mybag-header">
-                    <img src={bolsa} />
-                  </div>
-
-                  <div className="desc-product-mybag-header">
-                    <p>Coach</p>
-                    <p>Leather Coach Bag</p>
-                    <div className="box-counter-product-header">
-                      <button>-</button>
-                      <p>1</p>
-                      <button>+</button>
-                    </div>
-                  </div>
-
-                  <div className="price-product-header">
-                    <button>
-                      <img src={cross} alt="" />
-                    </button>
-                    <p>R$54.69</p>
-                  </div>
-                </div>
-                <hr className="line-product-header" />
-
-                <div className="mybag-container-product-header">
-                  <div className="img-product-mybag-header">
-                    <img src={bolsa} />
-                  </div>
-
-                  <div className="desc-product-mybag-header">
-                    <p>Coach</p>
-                    <p>Leather Coach Bag</p>
-                    <div className="box-counter-product-header">
-                      <button>-</button>
-                      <p>1</p>
-                      <button>+</button>
-                    </div>
-                  </div>
-
-                  <div className="price-product-header">
-                    <button>
-                      <img src={cross} alt="" />
-                    </button>
-                    <p>R$54.69</p>
-                  </div>
-                </div>
-                <hr className="line-product-header" />
-
-                <div className="mybag-container-product-header">
-                  <div className="img-product-mybag-header">
-                    <img src={bolsa} />
-                  </div>
-
-                  <div className="desc-product-mybag-header">
-                    <p>Coach</p>
-                    <p>Leather Coach Bag</p>
-                    <div className="box-counter-product-header">
-                      <button>-</button>
-                      <p>1</p>
-                      <button>+</button>
-                    </div>
-                  </div>
-
-                  <div className="price-product-header">
-                    <button>
-                      <img src={cross} alt="" />
-                    </button>
-                    <p>R$54.69</p>
-                  </div>
-                </div>
-                <hr className="line-product-header" />
-              </div>
-
-              <div className="total-price-header">
-                <div className="txt-price-header">
-                  <p className="sub-tax-header">Subtotal:</p>
-                  <p className="sub-tax-header">Tax:</p>
-                  <p className="final-price-header">Total:</p>
-                </div>
-
-                <div className="prices-header">
-                  <p className="sub-tax-header">$109.38</p>
-                  <p className="sub-tax-header">$2.00</p>
-                  <p className="final-price-header">$111.38</p>
-                </div>
-              </div>
-
-              <div className="container-btns-header">
-                <div className="coupon-header">
-                  <input type="text" placeholder="Apply Coupon Code" />
-                  <button>CHECK</button>
-                </div>
-
-                <button className="order-btn-header">Place Order</button>
-
-                <p>Continue Shopping</p>
-
-              </div>
-            </div>
-          )}
+          <MyBagModal hover={isHovered} />
 
           <div className="container-header-responsivo">
             <div className="logo-menu">
