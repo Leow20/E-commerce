fa5a16f64cf33abc262f1fe3352ef4d614e6602a
lucas-hack <luhcb12@gmail.com>
2023-07-20 16:23:06 -0300

    feat/mybag-mobile empty and button component

--- project-ecommerce/src/Pages/MyBag/index.jsx
+++ project-ecommerce/src/Pages/MyBag/index.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import "./myBag.css";
+
+import ButtonBigMob from "../../components/ButtonBigMobile";
+import arrow from "../../assets/icons/arrowProfile.svg";
+import empty from "../../assets/Img/emptyBag.png";
 
 const MyBag = () => {
-	return <div>MyBag</div>;
+  return (
+    <div className="window-mybag">
+      <div className="container-title-mybag">
+        <img src={arrow} />
+        <h3>My Bag</h3>
+      </div>
+
+      <div className="empty-mybag">
+        <img src={empty} />
+        <h2>Uh Oh....!</h2>
+        <p>
+          You haven’t added any any items. Start shopping to make your bag bloom
+        </p>
+      </div>
+
+      <div className="button-mybag">
+        <ButtonBigMob>Continue Shopping</ButtonBigMob>
+      </div>
+    </div>
+  );
 };
 
 export default MyBag;
