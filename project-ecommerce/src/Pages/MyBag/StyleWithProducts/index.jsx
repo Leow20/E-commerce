81db993d58349a869c6829e55521e782eb32b93a
lucas-hack <luhcb12@gmail.com>
2023-07-21 16:02:03 -0300

    chore/responsiveness improvements

--- project-ecommerce/src/Pages/MyBag/StyleWithProducts/index.jsx
+++ project-ecommerce/src/Pages/MyBag/StyleWithProducts/index.jsx
@@ -5,25 +5,27 @@ import close from "../../../assets/icons/cross.svg";
 
 function ContainerWP() {
   return (
-    <div className="productIn">
-      <div className="container-title-mybag">
-        <img src={close} />
-        <h3>My Bag</h3>
-      </div>
-
-      <div className="showProduct">
-        <h1>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam ipsam
-          vitae fuga atque veniam, exercitationem accusantium! quos
-        </h1>
-
-        <div className="coupon-input">
-          <input type="text" placeholder="Apply Coupon Code" />
-          <button>CHECK</button>
+    <>
+      <div className="productIn">
+        <div className="container-title-mybag">
+          <img src={close} />
+          <h3>My Bag</h3>
         </div>
 
+        <div className="showProduct">
+          <h1>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam
+            ipsam vitae fuga atque veniam, exercitationem accusantium! quos 
+          </h1>
+
+          <div className="coupon-input">
+            <input type="text" placeholder="Apply Coupon Code" />
+            <button>CHECK</button>
+          </div>
+        </div>
       </div>
-    </div>
+      <div className="img-circles"></div>
+    </>
   );
 }
 
