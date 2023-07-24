b375462817ec4c50e181e7b214a25c22668027fb
lucas-hack <luhcb12@gmail.com>
2023-07-24 17:46:33 -0300

    chore/more accurate positions

--- project-ecommerce/src/Pages/MyBag/index.jsx
+++ project-ecommerce/src/Pages/MyBag/index.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import "./myBag.css";
 
 import ButtonBigMob from "../../components/ButtonBigMobile";
@@ -8,6 +7,9 @@ import close from "../../assets/icons/cross.svg";
 import bolsa from "../../assets/Img/image.jpg";
 
 const MyBag = () => {
+
+  
+
   return (
     <div className="window-mybag">
       <div className="productIn">
@@ -18,6 +20,39 @@ const MyBag = () => {
 
         <div className="showProduct">
           <div>
+            <div className="box-product">
+              <div className="box-aling">
+                <img src={bolsa} />
+                <div className="info">
+                  <p>Coach</p>
+                  <p>Leather Coach Bag</p>
+                  <select>
+                    <option value="01">01</option>
+                    <option value="02">02</option>
+                    <option value="03">03</option>
+                    <option value="04">04</option>
+                    <option value="05">05</option>
+                    <option value="06">06</option>
+                    <option value="07">07</option>
+                    <option value="08">08</option>
+                    <option value="09">09</option>
+                    <option value="10">10</option>
+                  </select>
+
+                  <div className="prices">
+                    <p className="big-text">$54.69</p>
+                    <p className="small-text">$69.99</p>
+                    <p className="red-text">20% OFF</p>
+                  </div>
+                </div>
+              </div>
+              <hr className="line" />
+              <div className="buttons">
+                <button>Move to Wishlist</button>
+                <button>Remove</button>
+              </div>
+            </div>
+
             <div className="box-product">
               <div className="box-aling">
                 <img src={bolsa} />
@@ -119,6 +154,8 @@ const MyBag = () => {
           </div>
         </div>
       </div>
+
+
 
       {/* <div className="container-title-mybag">
         <img src={arrow} />
