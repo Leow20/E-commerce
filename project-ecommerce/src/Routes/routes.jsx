d1147636d99b667d97ac626d98d5031c7f70812b
William Teuschel Klein <williamklein08@gmail.com>
2023-07-25 09:36:31 -0300

    fix: deleting unnecessary routes

--- project-ecommerce/src/Routes/routes.jsx
+++ project-ecommerce/src/Routes/routes.jsx
@@ -4,11 +4,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 //Pages
 import About from "../Pages/About";
 import Home from "../Pages/Home";
-import Handbags from "../Pages/Handbags";
-import Watches from "../Pages/Watches";
-import Skincare from "../Pages/Skincare";
-import Jewellery from "../Pages/Jewellery";
-import Apparels from "../Pages/Apparels";
 import Categories from "../Pages/Categories";
 import Profile from "../Pages/Profile";
 import MyBag from "../Pages/MyBag";
@@ -20,28 +15,36 @@ import AddProduct from "../Pages/AddProduct";
 //Routes private
 import Private from "./private";
 
-
 const Router = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/categories" element={<Categories />} />
-        <Route path="/profile" element={<Private> <Profile /> </Private>} />
-        <Route path="/mybag" element={<MyBag />} />
-        <Route path="/handbags" element={<Handbags />} />
-        <Route path="/watches" element={<Watches />} />
-        <Route path="/skincare" element={<Skincare />} />
-        <Route path="/jewellery" element={<Jewellery />} />
-        <Route path="/apparels" element={<Apparels />} />
-        <Route path="/notification" element={<Notification />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/addproduct" element={<Private> <AddProduct /> </Private>} />
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/categories" element={<Categories />} />
+				<Route
+					path="/profile"
+					element={
+						<Private>
+							<Profile />
+						</Private>
+					}
+				/>
+				<Route path="/mybag" element={<MyBag />} />
+				<Route path="/notification" element={<Notification />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/signup" element={<SignUp />} />
+				<Route
+					path="/addproduct"
+					element={
+						<Private>
+							<AddProduct />
+						</Private>
+					}
+				/>
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default Router;
