a2fe792b51721e3da93fbbe0dbcd757549720a07
William Teuschel Klein <williamklein08@gmail.com>
2023-07-25 15:27:09 -0300

    feat: creation of the product data folder

---
+++ project-ecommerce/src/Pages/ProductData/index.jsx
@@ -0,0 +1,15 @@
+//React
+import React from "react";
+
+//Style
+import "./productData.css";
+
+//Firebase
+import { db } from "../../../firebaseConnection";
+import { getDocs } from "firebase/firestore";
+
+const ProductData = () => {
+	return <section></section>;
+};
+
+export default ProductData;
