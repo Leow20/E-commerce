c748c6beab69aff2f2f958b8699545587407cb48
Leonardo Winter <leonardo.2020305040@aluno.iffar.edu.br>
2023-07-26 17:51:34 -0300

    feat: get info with context

---
+++ project-ecommerce/src/Contexts/user.jsx
@@ -0,0 +1,47 @@
+import React, { createContext, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, db } from "../../firebaseConnection"; // Substitua pelo caminho correto
+import {
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  query,
+  where,
+} from "@firebase/firestore";
+
+export const UserContext = createContext({});
+
+function UserProvider({ children }) {
+  const [user, setUser] = useState("");
+
+  const fetchUserInfoAndUpdateState = async (uid) => {
+    try {
+      const q = query(collection(db, "users"), where("uid", "==", uid));
+
+      await getDocs(q).then((value) => {
+        value.forEach(async (valueInfo) => {
+          const userData = valueInfo.data();
+          setUser(userData);
+        });
+        console.log("teste");
+      });
+    } catch (error) {
+      console.error("Erro ao obter dados do Firestore:", error);
+    }
+  };
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      fetchUserInfoAndUpdateState(user.uid);
+    } else {
+      console.log("Não Autorizado");
+    }
+  });
+
+  return (
+    <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
+  );
+}
+
+export default UserProvider;
