5e3b59425e50c0ca760e1fe9d5ebe8b9ecb2b9d4
Patrick <patrickprestes977@gmail.com>
2023-07-27 10:58:34 -0300

    finished mobile version

--- project-ecommerce/src/components/ReferAndEarn/index.jsx
+++ project-ecommerce/src/components/ReferAndEarn/index.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import "./referEarn.css";
+import imgRefer from "../../assets/imgReferEarn.png";
 
 const ReferAndEarn = () => {
-    return <div>ReferAndEarn</div>;
+    return (
+        <div className="container-refer">
+            <div className="image-Refer">
+                <img src={imgRefer} alt="imgRefer" />
+            </div>
+            <div className="text-one">
+                <p>Invite your friend and earn $20 on every invite</p>
+            </div>
+            <div className="input-code">
+                <input type="text" id="code-text" />
+            </div>
+            <div className="text-two">
+                <p>Tap to copy the code.</p>
+            </div>
+            <div className="text-three">
+                <p>How does this works?</p>
+            </div>
+            <div className="box-text">
+                <ul>
+                    <li>Invite your friends to CORAL</li>
+                    <li>Ask your friends to place their order with your code & get $20 discount</li>
+                    <li>Once the order gets delivered you get the discount as well.</li>
+                </ul>
+            </div>
+            <button className="button-invite">Inivite now</button>
+
+        </div>
+    );
+
 };
 
 export default ReferAndEarn;
