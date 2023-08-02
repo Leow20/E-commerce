8fbdbe1d7d65cb8b9d3b6d8cfbac0b6f459e0e6a
William Teuschel Klein <williamklein08@gmail.com>
2023-08-02 10:48:37 -0300

    chore: Adjustment Drawer data

--- project-ecommerce/src/components/HeaderModal/index.jsx
+++ project-ecommerce/src/components/HeaderModal/index.jsx
@@ -1,5 +1,5 @@
 //React
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 //Style
 import "./headerModal.css";
@@ -11,139 +11,140 @@ import { Link, useNavigate } from "react-router-dom";
 import arrowBlack from "../../assets/HeaderModal/arrow-right-black.svg";
 import arrowGray from "../../assets/HeaderModal/arrow-right-gray.svg";
 import userNotPicture from "../../assets/HeaderModal/user-sem-foto.png";
+import { UserContext } from "../../Contexts/user";
 
 const HeaderModal = ({ isOpen, setIsOpen, id = "headerModalId" }) => {
-  const [animation, setAnimation] = useState(
-    "content-header-modal animation-header-modal"
-  );
+	const [animation, setAnimation] = useState(
+		"content-header-modal animation-header-modal"
+	);
 
-  const user = JSON.parse(localStorage.getItem("userLogado"));
-  const navigate = useNavigate("");
+	const { user } = useContext(UserContext);
+	const navigate = useNavigate("");
 
-  const handleBackClick = (e) => {
-    e.preventDefault();
+	const handleBackClick = (e) => {
+		e.preventDefault();
 
-    if (e.target.id !== id) {
-      return;
-    } else {
-      handleModalState();
-    }
-  };
-  addEventListener("scroll", () => handleModalState());
+		if (e.target.id !== id) {
+			return;
+		} else {
+			handleModalState();
+		}
+	};
+	addEventListener("scroll", () => handleModalState());
 
-  function handleModalState() {
-    setAnimation("content-header-modal animate-modal-info");
+	function handleModalState() {
+		setAnimation("content-header-modal animate-modal-info");
 
-    setTimeout(() => {
-      setIsOpen(false);
-      setAnimation("content-header-modal animation-header-modal");
-    }, 300);
-  }
+		setTimeout(() => {
+			setIsOpen(false);
+			setAnimation("content-header-modal animation-header-modal");
+		}, 300);
+	}
 
-  if (isOpen) {
-    return (
-      <div id={id} className="header-modal" onClick={handleBackClick}>
-        <div className={animation}>
-          <div className="profile-header-modal">
-            <button onClick={() => navigate("/profile")}>
-              <div>
-                <img
-                  src={user && user.URLfoto ? user.URLfoto : userNotPicture}
-                  alt="User"
-                  id="imgUserId"
-                />
-                <h1>
-                  Hello
-                  {user && user.firstName && user.lastName
-                    ? ", " + user.firstName + " " + user.lastName
-                    : ""}
-                </h1>
-              </div>
-              <img src={arrowBlack} alt="arrow-icon" />
-            </button>
-          </div>
-          <hr />
-          <nav className="categories-header-modal">
-            <h2>Top Categories</h2>
-            <ul>
-              <li>
-                <Link to="/skincare">
-                  <label>Skincare</label>
-                  <img src={arrowGray} alt="arrow-icon" />
-                </Link>
-              </li>
-              <li>
-                <Link to="/apparels">
-                  <label>Apparels</label>
-                  <img src={arrowGray} alt="arrow-icon" />
-                </Link>
-              </li>
-              <li>
-                <Link to="/jewellery">
-                  <label>Jewellery</label>
-                  <img src={arrowGray} alt="arrow-icon" />
-                </Link>
-              </li>
-              <li>
-                <Link to="/handbags">
-                  <label>Handbags</label>
-                  <img src={arrowGray} alt="arrow-icon" />
-                </Link>
-              </li>
-              <li>
-                <Link to="">
-                  <label>Eyeware</label>
-                  <img src={arrowGray} alt="arrow-icon" />
-                </Link>
-              </li>
-              <li>
-                <Link to="">
-                  <label>Fragrance</label>
-                  <img src={arrowGray} alt="arrow-icon" />
-                </Link>
-              </li>
-              <li>
-                <Link to="/watches">
-                  <label>Watches</label>
-                  <img src={arrowGray} alt="arrow-icon" />
-                </Link>
-              </li>
-              <li>
-                <Link to="/about">
-                  <label>About</label>
-                  <img src={arrowGray} alt="arrow-icon" />
-                </Link>
-              </li>
-            </ul>
-          </nav>
-          <hr />
-          <nav className="contact-header-mobile">
-            <h2>Contact Us</h2>
-            <ul>
-              <li>
-                <Link>
-                  <label>Help & Support</label>
-                  <img src={arrowBlack} alt="arrow-icon" />
-                </Link>
-              </li>
-              <li>
-                <Link>
-                  <label>Feedback & Suggestions</label>
-                  <img src={arrowBlack} alt="arrow-icon" />
-                </Link>
-              </li>
-              <li>
-                <Link>
-                  <label>Visit Websites</label>
-                  <img src={arrowBlack} alt="arrow-icon" />
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    );
-  }
+	if (isOpen) {
+		return (
+			<div id={id} className="header-modal" onClick={handleBackClick}>
+				<div className={animation}>
+					<div className="profile-header-modal">
+						<button onClick={() => navigate("/profile")}>
+							<div>
+								<img
+									src={user && user.url ? user.url : userNotPicture}
+									alt="User"
+									id="imgUserId"
+								/>
+								<h1>
+									Hello
+									{user && user.firstName && user.lastName
+										? ", " + user.firstName + " " + user.lastName
+										: ""}
+								</h1>
+							</div>
+							<img src={arrowBlack} alt="arrow-icon" />
+						</button>
+					</div>
+					<hr />
+					<nav className="categories-header-modal">
+						<h2>Top Categories</h2>
+						<ul>
+							<li>
+								<Link to="/skincare">
+									<label>Skincare</label>
+									<img src={arrowGray} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link to="/apparels">
+									<label>Apparels</label>
+									<img src={arrowGray} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link to="/jewellery">
+									<label>Jewellery</label>
+									<img src={arrowGray} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link to="/handbags">
+									<label>Handbags</label>
+									<img src={arrowGray} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link to="">
+									<label>Eyeware</label>
+									<img src={arrowGray} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link to="">
+									<label>Fragrance</label>
+									<img src={arrowGray} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link to="/watches">
+									<label>Watches</label>
+									<img src={arrowGray} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link to="/about">
+									<label>About</label>
+									<img src={arrowGray} alt="arrow-icon" />
+								</Link>
+							</li>
+						</ul>
+					</nav>
+					<hr />
+					<nav className="contact-header-mobile">
+						<h2>Contact Us</h2>
+						<ul>
+							<li>
+								<Link>
+									<label>Help & Support</label>
+									<img src={arrowBlack} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link>
+									<label>Feedback & Suggestions</label>
+									<img src={arrowBlack} alt="arrow-icon" />
+								</Link>
+							</li>
+							<li>
+								<Link>
+									<label>Visit Websites</label>
+									<img src={arrowBlack} alt="arrow-icon" />
+								</Link>
+							</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		);
+	}
 };
 
 export default HeaderModal;
