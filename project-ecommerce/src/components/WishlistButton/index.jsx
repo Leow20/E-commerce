ff340b574d7b3e4fffa5d1219d89800e7cd83094
Leonardo Winter <leonardo.2020305040@aluno.iffar.edu.br>
2023-08-05 12:04:50 -0300

    fix: infinite loop on wishlish

--- project-ecommerce/src/components/WishlistButton/index.jsx
+++ project-ecommerce/src/components/WishlistButton/index.jsx
@@ -11,59 +11,60 @@ import { db } from "../../../firebaseConnection";
 import { UserContext } from "../../Contexts/user";
 
 function WishlistButton({
-	product,
-	button = true,
-	className = "img-product-home-button",
-	text = "",
+  product,
+  button = true,
+  className = "img-product-home-button",
+  text = "",
 }) {
-	const { user } = useContext(UserContext);
-	const [wishlist, setWishlist] = useState([]);
+  const { user } = useContext(UserContext);
+  const [wishlist, setWishlist] = useState([]);
 
-	const handleWishlist = async () => {
-		if (!user || !product) return;
+  const handleWishlist = async () => {
+    if (!user || !product) return;
 
-		const index = wishlist.findIndex((item) => item.id === product.id);
-		const updatedWishlist = [...wishlist];
-		if (index === -1) {
-			updatedWishlist.push(product);
-		} else {
-			updatedWishlist.splice(index, 1);
-		}
+    const index = wishlist.findIndex((item) => item.id === product.id);
+    const updatedWishlist = [...wishlist];
+    if (index === -1) {
+      updatedWishlist.push(product);
+    } else {
+      updatedWishlist.splice(index, 1);
+    }
 
-		setWishlist(updatedWishlist);
-		console.log(updatedWishlist);
+    setWishlist(updatedWishlist);
+    console.log(updatedWishlist);
 
-		await setDoc(doc(db, "wishlist", user.uid), { data: updatedWishlist });
-	};
+    await setDoc(doc(db, "wishlist", user.uid), { data: updatedWishlist });
+  };
 
-	useEffect(() => {
-		const loadWishlist = async () => {
-			if (user) {
-				const wishlistRef = doc(db, "wishlist", user.uid);
-				const wishlistSnapshot = await getDoc(wishlistRef);
-				if (wishlistSnapshot.exists()) {
-					setWishlist(wishlistSnapshot.data().data);
-				} else {
-					setWishlist([]);
-				}
-			}
-		};
-		loadWishlist();
-	}, [user, wishlist]);
+  useEffect(() => {
+    console.log("aaaaaaa");
+    const loadWishlist = async () => {
+      if (user) {
+        const wishlistRef = doc(db, "wishlist", user.uid);
+        const wishlistSnapshot = await getDoc(wishlistRef);
+        if (wishlistSnapshot.exists()) {
+          setWishlist(wishlistSnapshot.data().data);
+        } else {
+          setWishlist([]);
+        }
+      }
+    };
+    loadWishlist();
+  }, [user]);
 
-	const isProductInWishlist = wishlist.some((item) => item.id === product.id);
-	const HeartIcon = isProductInWishlist ? AiFillHeart : AiOutlineHeart;
+  const isProductInWishlist = wishlist.some((item) => item.id === product.id);
+  const HeartIcon = isProductInWishlist ? AiFillHeart : AiOutlineHeart;
 
-	if (button) {
-		return (
-			<div>
-				<button onClick={handleWishlist} className={className}>
-					<HeartIcon id="imgHeartID" />
-					{text ? <span>{text}</span> : null}
-				</button>
-			</div>
-		);
-	}
+  if (button) {
+    return (
+      <div>
+        <button onClick={handleWishlist} className={className}>
+          <HeartIcon id="imgHeartID" fill="#1b4b66" />
+          {text ? <span>{text}</span> : null}
+        </button>
+      </div>
+    );
+  }
 }
 
 export default WishlistButton;
