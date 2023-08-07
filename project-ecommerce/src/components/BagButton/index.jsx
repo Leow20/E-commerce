b9aa30abc9c9ebc484cd941a6190f61449fd50fb
William Teuschel Klein <williamklein08@gmail.com>
2023-08-06 21:53:51 -0300

    feat: mybag functionality

--- project-ecommerce/src/components/BagButton/index.jsx
+++ project-ecommerce/src/components/BagButton/index.jsx
@@ -1,15 +1,68 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Bag from "../../assets/icons/bag-white.svg";
+import BagIcon from "../../assets/imgHeader/bag.svg";
 
-const BagButton = ({ product }) => {
-	return (
-		<div>
-			<button className="add-bag">
-				<img src={Bag} alt="Bag" />
-				<span>Add to Bag</span>
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { db } from "../../../firebaseConnection";
+import { UserContext } from "../../Contexts/user";
+import { toast } from "react-toastify";
+
+const BagButton = ({ product, className = "add-bag", qty = 1 }) => {
+	const { user } = useContext(UserContext);
+	const [bag, setBag] = useState([]);
+	if (product) {
+		product.qtyBag = qty;
+	}
+
+	const loadBag = async () => {
+		console.log("aaaaa");
+		const bagSnapshot = await getDoc(doc(db, "bag", user.uid));
+		if (user) {
+			if (bagSnapshot.exists()) {
+				setBag(bagSnapshot.data().data);
+			} else {
+				setBag([]);
+				await setDoc(doc(db, "bag", user.uid), { data: bag });
+			}
+		}
+	};
+
+	const handleBag = async () => {
+		if (!user || !product) return;
+		const bagtSnapshot = await getDoc(doc(db, "bag", user.uid));
+		const updatedBag = bagtSnapshot.data().data;
+		updatedBag.push(product);
+
+		setBag(updatedBag);
+		console.log(updatedBag);
+
+		await setDoc(doc(db, "bag", user.uid), { data: updatedBag }).then(() =>
+			toast.success("Product added to bag")
+		);
+	};
+
+	useEffect(() => {
+		console.log("bbb");
+		loadBag();
+	}, [user]);
+
+	const isProductInBag = bag.some((item) => item.id === product.id);
+	if (className == "add-bag") {
+		return (
+			<div>
+				<button className={className} onClick={handleBag}>
+					<img src={Bag} alt="Bag" />
+					<span>Add to Bag</span>
+				</button>
+			</div>
+		);
+	} else {
+		return (
+			<button className="button-bag-wishlist" onClick={handleBag}>
+				<img src={BagIcon} alt="Icone de sacola" /> Add to bag
 			</button>
-		</div>
-	);
+		);
+	}
 };
 
 export default BagButton;
