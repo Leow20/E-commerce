fa98ac31aef8f73bbb230595a43dbaffd7d73b57
William Teuschel Klein <williamklein08@gmail.com>
2023-08-08 16:03:18 -0300

    fix: css tweaks on error 404 page

--- project-ecommerce/src/Pages/Page404/index.jsx
+++ project-ecommerce/src/Pages/Page404/index.jsx
@@ -7,19 +7,19 @@ import ButtonBigMob from "../../components/ButtonBigMobile";
 import { Link } from "react-router-dom";
 
 function Erro404() {
-  return (
-    <div className="windown">
-      <div className="container-page404">
-        <img src={erro} alt="" />
-        <h2>Ops... this page is not found</h2>
-        <h4>click the button to go Back to Home</h4>
+	return (
+		<div className="windown">
+			<div className="container-page404">
+				<img src={erro} alt="Error 404" />
+				<h2>Ops... this page is not found</h2>
+				<h4>Click the button to go back to home</h4>
 
-        <Link to="/" className="button-page404">
-          <ButtonBigMob>Back to Home</ButtonBigMob>
-        </Link>
-      </div>
-    </div>
-  );
+				<Link to="/" className="button-page404">
+					<ButtonBigMob>Back to Home</ButtonBigMob>
+				</Link>
+			</div>
+		</div>
+	);
 }
 
 export default Erro404;
