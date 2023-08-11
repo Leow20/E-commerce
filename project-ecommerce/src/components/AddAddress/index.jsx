b0e781f9dabcddee24290c9afac613f1e8233b45
lucas-hack <luhcb12@gmail.com>
2023-08-11 00:07:11 -0300

    feat:some inputs and styles

--- project-ecommerce/src/components/AddAddress/index.jsx
+++ project-ecommerce/src/components/AddAddress/index.jsx
@@ -1,9 +1,26 @@
 import "./AddAddress.css";
 
+import arrow from "../../assets/icons/arrowProfile.svg";
+
 function AddAddress() {
   return (
     <div>
-      <h1>Add New Address</h1>
+      <header className="header-add-address">
+        <img src={arrow} />
+        <h1>Add New Address</h1>
+      </header>
+
+      <div className="info-add-address">
+        <p>Contact Information</p>
+        <hr className="line-add-address" />
+        <div className="info-add-address">
+          <input className="inputs-info-add-address" type="text" placeholder="Full Name" />
+          <div className="container-phone-add-address">
+            <input className="small-input-add-address" type="number" placeholder="+11" />
+            <input type="number" placeholder="Contact Number" />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
