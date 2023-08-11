304806c6fb9c2ca2ecd60c2cee51604be185d469
lucas-hack <luhcb12@gmail.com>
2023-08-11 14:59:45 -0300

    feat:more inputs and buttons

--- project-ecommerce/src/components/AddAddress/index.jsx
+++ project-ecommerce/src/components/AddAddress/index.jsx
@@ -4,7 +4,7 @@ import arrow from "../../assets/icons/arrowProfile.svg";
 
 function AddAddress() {
   return (
-    <div>
+    <>
       <header className="header-add-address">
         <img src={arrow} />
         <h1>Add New Address</h1>
@@ -14,14 +14,59 @@ function AddAddress() {
         <p>Contact Information</p>
         <hr className="line-add-address" />
         <div className="info-add-address">
-          <input className="inputs-info-add-address" type="text" placeholder="Full Name" />
+          <input
+            className="inputs-info-add-address"
+            type="text"
+            placeholder="Full Name"
+          />
           <div className="container-phone-add-address">
-            <input className="small-input-add-address" type="number" placeholder="+11" />
-            <input type="number" placeholder="Contact Number" />
+            <input
+              className="small-input-add-address"
+              type="number"
+              placeholder="+11"
+            />
+            <input
+              className="medium-input-add-address"
+              type="number"
+              placeholder="Contact Number"
+            />
           </div>
         </div>
       </div>
-    </div>
+
+      <div className="info-add-address">
+        <p>Delivery Address</p>
+        <hr className="line-add-address" />
+        <div className="info-add-address">
+          <input
+            className="inputs-info-add-address"
+            type="text"
+            placeholder="Pin Code"
+          />
+          <input
+            className="inputs-info-add-address"
+            type="text"
+            placeholder="Pin Code"
+          />
+          <input
+            className="inputs-info-add-address"
+            type="text"
+            placeholder="Pin Code"
+          />
+          <select name="" id="">
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select>
+        </div>
+
+        <div className="div-buttons-add-address">
+          <button>Home</button>
+          <button>Office</button>
+          <button>Other </button>
+        </div>
+
+      </div>
+    </>
   );
 }
 
